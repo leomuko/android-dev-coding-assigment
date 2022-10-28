e95eec0372fa9c6f455ae942b99be404ac32fbdb
leomuko <kezzieleo@gmail.com>
2022-10-28 06:26:33 +0300

    Implement Dependency Injection with dagger hilt

---
+++ app/src/main/java/com/ensibuuko/android_dev_coding_assigment/EnsibukoApplication.kt
@@ -0,0 +1,8 @@
+package com.ensibuuko.android_dev_coding_assigment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class EnsibukoApplication : Application()
