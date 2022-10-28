a221e4543ed7efbc72aab593f978a9f20be568e3
leomuko <kezzieleo@gmail.com>
2022-10-28 13:20:31 +0300

    Implement Comments List Adapter

---
+++ app/src/main/java/com/ensibuuko/android_dev_coding_assigment/features/comments/CommentAdapter.kt
@@ -0,0 +1,55 @@
+package com.ensibuuko.android_dev_coding_assigment.features.comments
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ensibuuko.android_dev_coding_assigment.data.CommentModel
+import com.ensibuuko.android_dev_coding_assigment.databinding.ItemCommentBinding
+
+class CommentAdapter : ListAdapter<CommentModel, CommentAdapter.CommentViewHolder>(CommentComparator()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
+        val binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CommentViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if(currentItem != null){
+            holder.bind(currentItem)
+        }
+    }
+
+    class CommentViewHolder(private val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(commentModel: CommentModel){
+            binding.apply {
+                itemCommentBody.text = commentModel.body
+                itemCommentEmail.text = commentModel.email
+                itemCommentName.text = commentModel.name
+            }
+        }
+    }
+
+    class CommentComparator : DiffUtil.ItemCallback<CommentModel>(){
+        override fun areItemsTheSame(oldItem: CommentModel, newItem: CommentModel): Boolean {
+            if(oldItem.body == newItem.body && oldItem.name == newItem.name){
+                return true
+            }
+
+            return false
+        }
+
+        override fun areContentsTheSame(oldItem: CommentModel, newItem: CommentModel): Boolean {
+            if (oldItem == newItem){
+                return true
+            }
+            return false
+        }
+
+    }
+
+}
